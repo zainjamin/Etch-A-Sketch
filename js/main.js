5f858ef1d5fcd4c3be1cfb78ac070e7c0ef2e883
Zain Salman <zainsalman04@gmail.com>
2022-07-03 02:23:40 -0400

    added slider to change grid size, added drawing on hover

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
-const grid = document.querySelector('.grid');
-grid.setAttribute('style', 'grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr);');
-for(let i = 0; i<16*16; i++){
-  const square = document.createElement('div');
-  square.style.border = '2px solid black';
-  grid.appendChild(square);
-}
+const grid = document.querySelector(".grid");
+const sizeText = document.querySelector("h3");
+const sizeSlider = document.querySelector("input");
+let size = 16;
+
+const colorSquare = (event) => {
+  const square = event.currentTarget;
+  square.style.backgroundColor = 'black';
+};
+
+const createGrid = () => {
+  grid.replaceChildren();
+  grid.setAttribute(
+    "style",
+    ` grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`
+  );
+  for (let i = 0; i < size ** 2; i++) {
+    const square = document.createElement("div");
+    square.addEventListener('mouseover', colorSquare);
+    grid.appendChild(square);
+  }
+};
+
+sizeSlider.oninput = function () {
+  sizeText.textContent = `${this.value}x${this.value}`;
+  size = this.value;
+  createGrid();
+};
+
+createGrid();
